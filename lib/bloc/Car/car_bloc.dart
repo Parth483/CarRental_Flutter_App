966b317cfbef1e0f4a5372afda2a13f56acf6f75
Parth Gamit <parthgamit483@gmail.com>
2025-06-01 20:20:59 +0530

    half bloc

---
+++ lib/bloc/Car/car_bloc.dart
@@ -0,0 +1,19 @@
+// import 'package:bloc/bloc.dart';
+// import 'package:carrental/bloc/Car/car_event.dart';
+// import 'package:carrental/bloc/Car/car_state.dart';
+// import 'package:carrental/domain/usecases/get_cars.dart';
+
+// class CarBloc extends Bloc<CarEvent, CarState> {
+
+
+//   CarBloc() {
+//     on<LoadCars>((event, emit) {
+//       emit(CarLoading());
+
+//       try{
+
+
+//       }
+//     });
+//   }
+// }
